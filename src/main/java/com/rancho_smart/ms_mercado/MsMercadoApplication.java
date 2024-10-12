c7201e9b81c9b36b9ca25d5449776077bc76ecfe
el-sebas-galeano <jsebastian-galeano@javeriana.edu.co>
2024-10-12 04:18:36 -0500

    :sparkles: feat: Creación de la Aplicación SpringBoot.

---
+++ src/main/java/com/rancho_smart/ms_mercado/MsMercadoApplication.java
@@ -0,0 +1,13 @@
+package com.rancho_smart.ms_mercado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsMercadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsMercadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rancho_smart/ms_mercado/MsMercadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rancho_smart.ms_mercado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsMercadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
