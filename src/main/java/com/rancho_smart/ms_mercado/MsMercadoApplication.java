a12e669548a93e977fc068911a9aee17c7c51610
el-sebas-galeano <jsebastian-galeano@javeriana.edu.co>
2024-10-21 03:33:17 -0500

    :wrench: chore: Configuración con Docker y Docker Compose

--- src/main/java/com/rancho_smart/ms_mercado/MsMercadoApplication.java
+++ src/main/java/com/rancho_smart/ms_mercado/MsMercadoApplication.java
@@ -2,8 +2,10 @@ package com.rancho_smart.ms_mercado;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MsMercadoApplication {
 
 	public static void main(String[] args) {
